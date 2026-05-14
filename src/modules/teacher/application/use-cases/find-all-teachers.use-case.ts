56d876818cf50f162aca5189c0c4793dbdf81cab
Guilherm3vrp <guilherm3vrp@gmail.com>
2026-05-14 15:03:22 -0300

    fix: removendo logs

--- src/modules/teacher/application/use-cases/find-all-teachers.use-case.ts
+++ src/modules/teacher/application/use-cases/find-all-teachers.use-case.ts
@@ -12,7 +12,6 @@ export class FindAllTeachersUseCase {
 
   async execute(query: FilterFindAllTeachersDto) {
     try {
-      console.log('query log', query);
       const { teachers, total } = await this.teacherRepository.findAll(
         query.page,
         query.limit,
