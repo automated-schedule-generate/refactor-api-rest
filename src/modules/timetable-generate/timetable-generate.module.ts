5c481074845d4236d6e312da31a1f4c3035632c9
Guilherm3vrp <guilherm3vrp@gmail.com>
2026-06-15 22:07:30 -0300

    fix: removendo import não utilizado

--- src/modules/timetable-generate/timetable-generate.module.ts
+++ src/modules/timetable-generate/timetable-generate.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@database/database.module';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { TimetableGenerateModel } from '@models';
 import { TimetableGenerateController } from '@controllers';
 import { TimetableGenerateRepository } from '@repositories';
 import { TimetableGenerateRepositoryImpl } from '@repositories.impl';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    SequelizeModule.forFeature([TimetableGenerateModel]),
-  ],
+  imports: [DatabaseModule, SequelizeModule.forFeature([])],
   controllers: [TimetableGenerateController],
   providers: [
     {
