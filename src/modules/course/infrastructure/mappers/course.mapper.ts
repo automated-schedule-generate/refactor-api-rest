3eefca6d494cea9201ac92a2168a415353afb4dc
Guilherm3vrp <guilherm3vrp@gmail.com>
2026-06-17 22:50:33 -0300

    fix: pequenas alterações incompletas

--- src/modules/course/infrastructure/mappers/course.mapper.ts
+++ src/modules/course/infrastructure/mappers/course.mapper.ts
@@ -1,13 +1,27 @@
-import { CourseEntity } from '@entities';
+import { CourseEntity, TimetableEntryEntity } from '@entities';
 import { CourseModel } from '@models';
 
 export class CourseMapper {
-  static toEntity(model: CourseModel): CourseEntity {
-    return new CourseEntity(
+  static toEntity(
+    model: CourseModel,
+    more_data?: { timetable_entries?: TimetableEntryEntity[] },
+  ): CourseEntity {
+    const course = new CourseEntity(
       model.id,
       model.name,
       model.total_semesters,
       model.class_time,
     );
+
+    if (more_data) {
+      if (
+        more_data?.timetable_entries &&
+        more_data.timetable_entries.length > 0
+      ) {
+        course.timetable_entries = more_data.timetable_entries;
+      }
+    }
+
+    return course;
   }
 }
